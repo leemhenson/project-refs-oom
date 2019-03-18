f5c01f10804b1ca9a0c870d266042839e2540e7d
Lee Henson <lee.m.henson@gmail.com>
2019-03-18 09:52:15 +0000

    Bit of extra flesh on the bones

---
+++ README.md
@@ -0,0 +1,7 @@
+=== Project references out of memory error
+
+Open `stacks/project-one/src/app.ts`
+Observe tsserver chomping memory like it's going out of fashion.
+Toggle the comments on the two lines defining v1.
+Restart Typescript
+Observe tsserver behaving normally.
--- stacks/project-one/src/app.ts
+++ stacks/project-one/src/app.ts
@@ -2,5 +2,10 @@ import * as t from "io-ts";
 import { Option } from "fp-ts/lib/Option";
 import { makeSharedOption } from "@shared/lib/options";
 
+// Does not cause OOM:
+// export const v1 = makeSharedOption(false);
+
+// Does
 export const v1: Option<Date> = makeSharedOption(false);
+
 export const v2 = t.type({ a: t.number });
